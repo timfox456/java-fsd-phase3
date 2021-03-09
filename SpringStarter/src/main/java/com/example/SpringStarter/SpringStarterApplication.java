4ae82e7eda637828315bf76b7cfd6500113a4f68
Tim Fox <tim@elephantscale.com>
2021-03-09 17:01:01 -0500

    Added SpringStarter Project

---
+++ SpringStarter/src/main/java/com/example/SpringStarter/SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterApplication.class, args);
+	}
+
+}
---
+++ SpringStarter/src/test/java/com/example/SpringStarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

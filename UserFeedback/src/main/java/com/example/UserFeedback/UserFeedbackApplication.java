1d21efccbe7c041e734f83a9baac0f33a4e0fec3
Tim Fox <tim@elephantscale.com>
2021-03-15 15:44:19 -0400

    Added UserFeedback

---
+++ UserFeedback/src/main/java/com/example/UserFeedback/UserFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeedbackApplication.class, args);
+	}
+
+}
---
+++ UserFeedback/src/test/java/com/example/UserFeedback/UserFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

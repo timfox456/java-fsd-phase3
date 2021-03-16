53ff6ba16c7e6940bc0faf88755f42be799a84d7
Tim Fox <tim@elephantscale.com>
2021-03-16 17:27:50 -0400

    Added post for userfeedback

--- UserFeedback/src/main/java/com/example/UserFeedback/controllers/FeedbackController.java
+++ UserFeedback/src/main/java/com/example/UserFeedback/controllers/FeedbackController.java
@@ -3,6 +3,8 @@ package com.example.UserFeedback.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,13 @@ public class FeedbackController {
         // This returns a JSON or XML with the Feedbacks
         return feedbackService.GetAllFeedback();
     }
+	
+	@PostMapping("/feedback")
+	public Feedback addNewFeedback(@RequestBody Feedback newFeedback) {
+		// TODO: Do something useful here.  
+		// Add the Feedback.
+		return new Feedback();  // Change this.
+	}
 	
 
 }

d878d3311459fbabb1d55bc696fac676a94c962e
Tim Fox <tim@elephantscale.com>
2021-03-18 17:37:46 -0400

    Added some additional things to userfeedback

--- UserFeedback/src/main/java/com/example/UserFeedback/controllers/TestFormController.java
+++ UserFeedback/src/main/java/com/example/UserFeedback/controllers/TestFormController.java
@@ -1,5 +1,25 @@
 package com.example.UserFeedback.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class TestFormController {
 
+	
+	 @GetMapping(value="/testform")
+     public String showTestForm(ModelMap model) {
+		 //TODO: Create a JSP called "testformjsp.jsp"  
+		 return "testformjsp";
+     }
+	 
+	// @PostMapping("/testform")
+	// TODO: implement form submission
+	// TODO: add View JSP
+	// TODO: Call RestTemplate and make POST json request to localhost:8090/feedback
 }
